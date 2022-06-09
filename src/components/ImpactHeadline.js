f63b99ad7f9734b5765c84b59e2e5d93f8e1eb8e
klmrmt <klmrmt99@gmail.com>
2022-06-09 16:55:25 -0500

    impact headline is responsive now

--- src/components/ImpactHeadline.js
+++ src/components/ImpactHeadline.js
@@ -6,26 +6,26 @@ export default class Impactheadline extends Component {
     render() {
         return ( 
             <div className='background-impact align-items-center d-flex'>
-               <Container className=''>
+               <Container className='impactBox'>
                    <h1 className='impact-header'>
                         <p style={{display: 'inline', color: 'white', backgroundColor: '#FF552E', paddingLeft: 5, paddingRight: 5, marginRight: 15}}></p> Impact Area Headline
                    </h1>
                    <Row >
-                       <Col>
+                       <Col lg={6} sm={12}>
                             <h1 className='impact-title'>Agriculture</h1>
                             <p className='impact-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac metus est. Pellentesque eros urna, convallis non ullamcorper id, luctus a augue. Vivamus sit amet lectus id justo lacinia elementum. Integer vel mi porttitor, pulvinar erat nec, iaculis sapien. </p>
                        </Col>
-                       <Col>
+                       <Col lg={6} sm={12}> 
                             <h1 className='impact-title'>Education</h1>
                             <p className='impact-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac metus est. Pellentesque eros urna, convallis non ullamcorper id, luctus a augue. Vivamus sit amet lectus id justo lacinia elementum. Integer vel mi porttitor, pulvinar erat nec, iaculis sapien. </p>
                        </Col>
                    </Row>
                    <Row>
-                       <Col>
+                       <Col lg={6} sm={12}>
                             <h1 className='impact-title'>Finance</h1>
                             <p className='impact-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac metus est. Pellentesque eros urna, convallis non ullamcorper id, luctus a augue. Vivamus sit amet lectus id justo lacinia elementum. Integer vel mi porttitor, pulvinar erat nec, iaculis sapien. </p>
                        </Col>
-                       <Col>
+                       <Col lg={6} sm={12}>
                             <h1 className='impact-title'>Healthcare</h1>
                             <p className='impact-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac metus est. Pellentesque eros urna, convallis non ullamcorper id, luctus a augue. Vivamus sit amet lectus id justo lacinia elementum. Integer vel mi porttitor, pulvinar erat nec, iaculis sapien. </p>
                        </Col>
